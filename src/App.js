f86ddf6dd2712af8486572e839cf9827ad26b979
Ahmed Keshka <up962500@myport.ac.uk>
2024-05-11 07:50:32 +0300

    added webpack configuration for easy development

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ function App() {
   return (
     <>
       <h2>Website Blocker</h2>
-      <button>View blocked websites</button>
-      <button>Add website to blocked list</button>
-      <button>Remove website from blocked list</button>
+      <div className="container">
+        <button>View blocked websites</button>
+        <button>Add website to blocked list</button>
+        <button>Remove website from blocked list</button>
+      </div>
     </>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.svg$/i,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "popup.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public",
+        },
+      ],
+    }),
+  ],
+};
