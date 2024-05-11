e7c1b658991a6d87432368d5765583044bedc437
Ahmed Keshka <up962500@myport.ac.uk>
2024-05-11 05:59:54 +0300

    set up chrome extension boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Website Blocker</h2>
+      <button>View blocked websites</button>
+      <button>Add website to blocked list</button>
+      <button>Remove website from blocked list</button>
+    </>
   );
 }
 
